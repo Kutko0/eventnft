ecd275803e144aeec1e8d9b76ba2adee2328c05e
Erik Petra <erikpetra11@gmail.com>
2021-11-20 05:14:14 +0100

    added public folder to accommodate static files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const eventRoutes = require("./routes/eventRoutes");
 const app = express();
 
 //app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
     console.log("kokot");
