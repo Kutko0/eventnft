d3cf67de928ec6cc57e759c668f89163fe3cb343
Erik Petra <erikpetra11@gmail.com>
2021-11-21 15:15:12 +0100

    removed unnecessary postman docs and models

--- models/event.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Event {
-    constructor(eventName, quests, cid) {
-        this.eventName = eventName;
-        this.quests = quests;
-        this.cid = cid;
-    }
-};
--- models/quest.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Quest {
-    constructor(questName, task, reward) {
-        this.questName = questName;
-        this.task = task;
-        this.reward = reward;
-    }
-};
